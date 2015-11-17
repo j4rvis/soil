ee23f08062792f6bd8cd36881b8cf2768bfb9fb3
Your Git Name <example@mail.com>
2015-11-17 14:43:55 +0100

    Add singing keys to install spotify.

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 sudo apt-add-repository -y "deb http://ftp.de.debian.org/debian wheezy main "
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8B48AD6246925553
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7638D0442B90D010
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6FB2A1C265FFB764
+
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 
