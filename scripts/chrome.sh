32a480a2b318448d94dd39a4cf615ec6206a4f16
Michael Schwarz <schwarzm90@gmail.com>
2015-11-17 14:46:45 +0100

    Add chromium installation.

--- scripts/chrome.sh
+++ scripts/chrome.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-if [ ! -f /etc/apt/sources.list.d/google.list ]; then
-  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
-fi
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg chromium-codecs-ffmpeg-extra
